eeeafd1941d317a761963286f5ceda205d279793
ThomasBr75 <thomas.brekken@timpex.no>
2011-07-05 12:58:41 +0200

    First Customer commit

---
+++ CustomerProject/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomerProject
+{
+    class Customer
+    {
+        private string _customerName;
+        private string _deliveryAddress;
+        private string _billingAddress;
+        private string _contact; 
+
+
+    }
+}
